df933ce018431f859d4ba550633aef33ee1b08ae
steady <inchang0326@naver.com>
2025-10-20 22:36:32 +0900

    only from gateway-server

---
+++ eureka-server/src/main/kotlin/com/teady/eurekaserver/filter/FromGatewayFilter.kt
@@ -0,0 +1,30 @@
+package com.teady.eurekaserver.filter
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class FromGatewayFilter(
+    @Value("\${gateway.secret}") private val gatewaySecret: String
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val secretHeader = request.getHeader("X-GATEWAY-SECRET")
+
+        if (secretHeader == null || secretHeader != gatewaySecret) {
+            response.status = HttpServletResponse.SC_FORBIDDEN
+            response.writer.write("Access Denied: Invalid")
+            return
+        }
+
+        filterChain.doFilter(request, response)
+    }
+}
