9492b8980f4ad5d5b4f5fe30075eb9a6f31209e3
steady <inchang0326@naver.com>
2025-10-20 15:21:17 +0900

    cors config to gateway-server

---
+++ gateway-server/src/main/kotlin/com/teady/gatewayserver/filter/CorsFilter.kt
@@ -0,0 +1,26 @@
+package com.teady.gatewayserver.filter
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+
+@Configuration
+class CorsFilter {
+
+    @Bean
+    fun corsWebFilter(): CorsWebFilter {
+        val corsConfig = CorsConfiguration()
+        corsConfig.allowedOrigins = listOf("http://localhost:5173")
+        corsConfig.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        corsConfig.addAllowedHeader("*")
+        corsConfig.allowCredentials = true
+
+        val source = UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", corsConfig)
+        }
+
+        return CorsWebFilter(source)
+    }
+}
